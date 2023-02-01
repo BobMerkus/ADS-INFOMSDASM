d7841b2fd8716f838400560af0053deac2a7dfb9
BobMerkus <bob.merkus@gmail.com>
2023-02-01 12:43:59 +0100

    .

--- Interpolate.py
+++ Interpolate.py
@@ -3,9 +3,10 @@ from PIL import Image
 from data_import import data_import_emissions_companies
 
 # Interpolate company emissions
+data = 'company_emission'
 dataset = data_import_emissions_companies([1990, 1995, 2000, 2005, 2010, 2015, 2020])
 interpolated = []
-ds_matrix = [v for k,v in dataset['company_count'].items()]
+ds_matrix = [v for k,v in dataset[data].items()]
 for y in range(len(ds_matrix[0])):
     row = []
     for x in range(len(ds_matrix[0][0])): #iterate over columns
@@ -17,7 +18,7 @@ interpolated = np.transpose(interpolated, (2, 0, 1)) #transpose to get correct s
 year = 1990
 for img in interpolated:
     result = Image.fromarray(img.astype(np.uint8))
-    result.save(f"Results/company_count_{year}_1000x1000.tif")
+    result.save(f"Results/{data}_{year}_interpolated_1000x1000.tif")
     year +=1
 
 # Visualise
@@ -26,8 +27,8 @@ from matplotlib import pyplot as plt
 fig, ax = plt.subplots(nrows=1, ncols=2)
 # Plot the first bar in the first subplot (interpolated)
 ax[0].bar(range(1990, 2026), [x.mean() for x in interpolated])
-ax[0].set_title('Interpolated Emissions')
+ax[0].set_title(f'Interpolated {data}')
 # Plot the second bar in the second subplot (original)
-ax[1].bar(dataset['company_emission'].keys(), [x.mean() for x in dataset['company_emission'].values()])
-ax[1].set_title('Original Emissions')
+ax[1].bar(dataset[data].keys(), [x.mean() for x in dataset[data].values()])
+ax[1].set_title(f'Original {data}')
 plt.show()
